f57de85672f39f77ebfa456889cd610175ef10f6
fabiom83 <fabiomorota@gmail.com>
2024-10-24 16:47:34 -0300

    Botão Consultar Vendas em listagemVIEW funcional. Abrindo janela e listando produtos vendidos

--- src/vendasVIEW.java
+++ src/vendasVIEW.java
@@ -18,7 +18,7 @@ public class vendasVIEW extends javax.swing.JFrame {
      */
     public vendasVIEW() {
         initComponents();
-       // listarProdutos();
+        listarProdutosVendidos();
     }
 
     /**
@@ -137,14 +137,14 @@ public class vendasVIEW extends javax.swing.JFrame {
     private javax.swing.JTable listaVendas;
     // End of variables declaration//GEN-END:variables
 
-    private void listarProdutos(){
+    private void listarProdutosVendidos(){
         try {
             ProdutosDAO produtosdao = new ProdutosDAO();
             
             DefaultTableModel model = (DefaultTableModel) listaVendas.getModel();
             model.setNumRows(0);
             
-            ArrayList<ProdutosDTO> listagem = produtosdao.listarProdutos();
+            ArrayList<ProdutosDTO> listagem = produtosdao.listarProdutosVendidos();
             
             for(int i = 0; i < listagem.size(); i++){
                 model.addRow(new Object[]{
